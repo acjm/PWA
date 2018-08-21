0ef9dc5a6a1b614ea3ba69f0a3110b2ca220d7ba
chafai <chafaiayman@gmail.com>
2018-08-21 14:13:29 +0100

    -  add source select
     - add a new news source select box - fetching api of news sources

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const api = '62b4debbbf7547f480258f3a583616f1';
 const main = document.querySelector('main');
+const selector = document.querySelector('#sourceSelector');
+const defaultSource = 'the-washington-post';
 window.addEventListener('load', e =>{
     updateNews();
+    updateSources();
 });
-
-async function updateNews(){
-    const res = await fetch (`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${api}`);
+// get the news 
+async function updateNews(source = defaultSource) {
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${api}`);
     const json = await res.json();
 
     main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+// get the sources 
+async function updateSources() {
+    const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${api}`);
+    const json = await res.json();
 
+    selector.innerHTML = json.sources
+    .map(src=>`<option value="${src.id}">${src.name}</option>`)
+    .join('\n');
 }
 
+//create articles
 function createArticle(article){
 
     return `
